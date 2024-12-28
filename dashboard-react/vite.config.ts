4d52af90c07664484148ca177fb2169a44e297e7
Juan Ballesteros <juanballesteros2001@gmail.com>
2024-12-28 13:13:55 -0500

    Add: vite pwa too intall app
    What: add sw to install app
    Why: noot use a link
    
    AG-24

--- dashboard-react/vite.config.ts
+++ dashboard-react/vite.config.ts
@@ -1,7 +1,48 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        name: "My Agenda",
+        short_name: "Agenda",
+        description: "Application to management appointments and clients",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#000000",
+        theme_color: "#000000",
+        icons: [
+          {
+            src: "/icon-64x64.png",
+            sizes: "64x64",
+            type: "image/png"
+          },
+          {
+            src: "/icon-448x448.png",
+            sizes: "448x448",
+            type: "image/png"
+          }
+        ]
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: ({ request }) => request.destination === "document",
+            handler: "NetworkFirst",
+            options: {
+              cacheName: "html-cache"
+            }
+          }
+        ]
+      }
+    })
+  ]
+});
