9a0087f11a31a665a64809b2847514163620dad1
PablitoCortes <cjuan1597@gmail.com>
2025-01-10 17:53:25 -0500

    Add sidebar AG-33
    What: added sidebar component with routing buttons (no routing yet)
    why: to start providiing visivility
    
    AG-33

--- dashboard-react/src/components/SideBar.tsx
+++ dashboard-react/src/components/SideBar.tsx
@@ -1,7 +1,24 @@
-export const Sidebar: React.FC = () => {
-  return (
-    <div>
-      <h1>Sidebar</h1>
+import React from'react'
+import './SideBar.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser, faCalendar, faX } from '@fortawesome/free-solid-svg-icons'
+
+export const Sidebar: React.FC<{ isOpen: boolean, toggleSidebar: () => void }> = ({isOpen}) => {
+  if (!isOpen) return null;
+      return (
+      <div className='menu-container'>
+      <div className='menu-content'>
+      <button><FontAwesomeIcon icon ={faX}/></button>
+        <ul className='sidebar'>
+          <li>Clientes
+            <FontAwesomeIcon icon={faUser}/>
+          </li>
+          <li>Calendario
+            <FontAwesomeIcon icon={faCalendar}/>
+          </li>
+        </ul>
+      </div>
     </div>
-  )
-}
+    )
+  }
+
--- dashboard-react/src/layouts/MainLayout.tsx
+++ dashboard-react/src/layouts/MainLayout.tsx
@@ -1,14 +1,23 @@
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faX } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
+import { Sidebar } from "../components/SideBar";
 
 export const MainLayout: React.FunctionComponent<React.PropsWithChildren> = ({
   children
 }) => {
+
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+   const toggleSidebar = () => { setIsSidebarOpen(!isSidebarOpen); };  
+
   return (
     <>
       <header className="flex py-4 shadow-md z-10 min-w-[250px]">
         <nav className="flex w-full justify-between items-center max-w-[700px] min-w-[250px] m-auto px-5">
-          <FontAwesomeIcon icon={faBars} />
+          <button onClick={toggleSidebar}>
+          {isSidebarOpen ?  <FontAwesomeIcon icon={faX} /> : <FontAwesomeIcon icon={faBars} />}
+          </button>
+          <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />          
           <h2>Angy Gonzaléz</h2>
         </nav>
       </header>
